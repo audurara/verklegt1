c8425a01c77be609fb31583d41c5a0bdd9c3e9d3
Tryggvith <Tryggvia16@ru.is>
2016-11-30 14:08:44 +0000

    created a vector funct for Info.txt in AataAccess (tilraun 2)

--- dataaccess.h
+++ dataaccess.h
@@ -15,18 +15,8 @@ class DataAccess
 
 public:
 
-<<<<<<< HEAD
-    /*
-    void writeData (parameters)
-=======
-    void readData();
->>>>>>> ade606abde5429e33630c34b1b3458f5cbfab663
-
     void writeData();
 
-    /*
-    vector <string> computerguys
-    */
     DataAccess();
     DataAccess(char name);
     vector<string> readData();
